068dfcf565f3258a7ddd758e51b5a952037f387f
Pratyush Kumar <pratyush3030gmail.compratyush3030gmail.com>
2025-08-27 21:33:57 +0530

    all  backend done in ts file

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,24 @@
-function sayHello(name:string){
-    console.log(`hello ${name}`)
-}
+require("dotenv").config()
+const express = require("express");
+const app = express();
+const PORT= process.env.PORT ||3000;
+const todoRoute=require('./routes/todos');
 
-sayHello("World")
-console.log("noedemon config")
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+
+// export  process.env.myName='dasds'
+console.log('My name is ',process.env.myName)
+console.log('MONGO_URL is ',process.env.MONGO_URL)
+
+app.get("/hello", (req:any, res:any) => {
+    res.send("hello world");
+});
+
+app.use('/public',express.static(__dirname+"/public"));
+
+app.use('/todos',todoRoute);
+app.get('/',(req:any,res:any)=>res.send('hello from backend'));
+
+
+app.listen(PORT, () => console.log("server staretd on "+PORT));
